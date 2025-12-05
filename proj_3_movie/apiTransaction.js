3cf1443da51a63003ce68b6b8f017aa99d03b3d8
ashu211103 <161711321+ashu211103@users.noreply.github.com>
2025-12-05 23:38:38 +0530

    Update apiTransaction.js

--- proj_3_movie/apiTransaction.js
+++ proj_3_movie/apiTransaction.js
@@ -5,34 +5,37 @@ const MOVIE_DB_IMAGE_ENDPOINT = 'https://image.tmdb.org/t/p/w500';
 const DEFAULT_POST_IMAGE = 'https://via.placeholder.com/150';
 
 function requestMovies(url, onComplete, onError) {
-    fetch(url)
+    fetch(url, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Accept": "application/json",
+        }
+    })
         .then((res) => res.json())
         .then(onComplete)
         .catch(onError);
 }
 
 function generateMovieDBUrl(path) {
-    const url = `${MOVIE_DB_ENDPOINT}/3${path}?api_key=${MOVIE_DB_API}`;
-    return url;
+    return `${MOVIE_DB_ENDPOINT}/3${path}?api_key=${MOVIE_DB_API}`;
 }
-
 
 function getTopRatedMovies() {
     const url = generateMovieDBUrl(`/movie/top_rated`);
-    const render = renderMovies.bind({ title: 'Top Rated Movies' })
+    const render = renderMovies.bind({ title: 'Top Rated Movies' });
     requestMovies(url, render, handleGeneralError);
 }
 
 function getTrendingMovies() {
     const url = generateMovieDBUrl('/trending/movie/day');
-    const render = renderMovies.bind({ title: 'Trending Movies' })
+    const render = renderMovies.bind({ title: 'Trending Movies' });
     requestMovies(url, render, handleGeneralError);
 }
 
-
 function searchUpcomingMovies() {
     const url = generateMovieDBUrl('/movie/upcoming');
-    const render = renderMovies.bind({ title: 'Upcoming Movies' })
+    const render = renderMovies.bind({ title: 'Upcoming Movies' });
     requestMovies(url, render, handleGeneralError);
 }
 
@@ -42,12 +45,10 @@ function searchPopularMovie() {
     requestMovies(url, render, handleGeneralError);
 }
 
-// Invoke a different function for search movies
 function searchMovie(value) {
-    const url = generateMovieDBUrl('/search/movie') + '&query=' + value;
+    const url = `${generateMovieDBUrl('/search/movie')}&query=${encodeURIComponent(value)}`;
     requestMovies(url, renderSearchMovies, handleGeneralError);
 }
-
 
 function getVideosByMovieId(movieId, content) {
     const url = generateMovieDBUrl(`/movie/${movieId}/videos`);
